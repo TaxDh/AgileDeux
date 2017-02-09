43c87dc370ec11b27d86d6d5331b26d2e3073843
kf891141 <kf891141@C39041.ens.uqam.ca>
2017-02-09 17:56:17 -0500

    creation main

---
+++ src/projetsession/ProjetSession.java
@@ -0,0 +1,17 @@
+/* main du projet*/
+package projetsession;
+
+/**
+ *
+ * @author kf891141
+ */
+public class ProjetSession {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
