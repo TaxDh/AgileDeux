f9c5f87be47daba60eb0e4ec8c6d8912e9a0cdda
kf891141 <kf891141@C39041.ens.uqam.ca>
2017-02-09 18:50:34 -0500

    commentaire pour Classe MethodeSoins

--- src/projetsession/MethodeSoins.java
+++ src/projetsession/MethodeSoins.java
@@ -11,5 +11,5 @@ package projetsession;
 public abstract class MethodeSoins {
     /*méthode pour chaque type de soin défini de manière générale pour 
     chaque contrat*/
-    
+    //parametre un int pour le numero et le cout, je pense
 }
