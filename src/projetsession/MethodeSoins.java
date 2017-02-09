896c1cd85d8ed3bf47a3390d09b6cd8d2d09c15f
kf891141 <kf891141@C39041.ens.uqam.ca>
2017-02-09 18:13:50 -0500

    Classe abstraite pour traiter les méthodes (type de soin) de chaque contrat

---
+++ src/projetsession/MethodeSoins.java
@@ -0,0 +1,13 @@
+/* chaque soin est considéré comme une méthode
+elle différe pour chaque objet (contrat), donc on utilise une classe
+abstraite.
+ */
+package projetsession;
+
+/**
+ *
+ * @author kf891141
+ */
+public abstract class MethodeSoins {
+    
+}
