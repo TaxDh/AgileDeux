d18c05e7a24ba2dfbd61c62b0aa3d6a539593db0
TaxDh <dhmo123@hotmail.com>
2017-02-12 01:25:34 -0500

    4 premiere methode de fait, pour faire les premiers test

--- src/projetsession/ContratA.java
+++ src/projetsession/ContratA.java
@@ -8,9 +8,26 @@ package projetsession;
  * @author kf891141
  */
 public class ContratA {
+    
+    //numero 0
     public double massotherapie(double montant){
-    return 0.0;
+    return montant * 0.25;
     }
     
-   // public abst
+    //numero 100
+    public double osteopathie(double montant){
+        return montant * 0.25; 
+    }
+    
+    //numero 200
+    public double psychologie_individuelle(double montant){
+        return montant * 0.25;
+    }
+    
+    //numero 300 a 399
+    public double soin_dentaire(double montant){
+        return 0.0;
+    }
+    
+   
 }
