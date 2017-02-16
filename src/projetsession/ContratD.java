b3b0864392f3c324a545c788436e344dfab7a7d9
hp <hp@192.168.2.16>
2017-02-15 22:13:29 -0800

    reste des traitements pour le contrat D

--- src/projetsession/ContratD.java
+++ src/projetsession/ContratD.java
@@ -22,55 +22,83 @@ public class ContratD extends InterfaceContrat{
     //numero 0
     public String massotherapie(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.25;
+        if(montantDouble <= 85){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 85;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 100
     public String osteopathie(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.25;
+       if(montantDouble <= 75){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 75;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 200
     public String psychologie_individuelle(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.25;
+        if(montantDouble <= 100){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 100;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 300 a 399
     public String soin_dentaire(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.0;
+        montantDouble = montantDouble * 0.100;
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 400   
     public String naturo_acup(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.0;
+        if(montantDouble <= 65){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 65;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 500
     public String chiropratie(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.25;
+        if(montantDouble <= 65){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 65;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     
     //numero 600
     public String physiotherapie(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.40;
+        if(montantDouble <= 100){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 100;
+        }
         return convertirDoubleEnString(montantDouble);
     }
     //numero 700
     public String ortho_ergo(String montant){
         double montantDouble = convertirStringEnDouble(montant);
-        montantDouble = montantDouble * 0.0;
+        if(montantDouble <= 100){
+            montantDouble = montantDouble * 0.100;
+        } else {
+            montantDouble = 90;
+        }
         return convertirDoubleEnString(montantDouble);
     }
    
