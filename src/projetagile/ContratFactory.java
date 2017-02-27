66bbc6edfe6ba2d0f1c90fb787def483684da31c
TaxDh <dhmo123@hotmail.com>
2017-02-26 21:16:39 -0500

    o

---
+++ src/projetagile/ContratFactory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetagile;
+
+import projetagile.jsonmodels.ModeleJsonIn;
+
+/**
+ *
+ * @author rene
+ */
+public class ContratFactory {
+    public static InterfaceContrat instancieContrat(ModeleJsonIn modele){
+        char typeContrat = modele.getTypeContrat();
+        InterfaceContrat contractHolder = null;
+        switch(typeContrat){
+            case 'A':
+                contractHolder = new ContratA(modele);
+                break;
+            case 'B':
+                contractHolder = new ContratB(modele);
+                break;
+            case 'C':
+                contractHolder = new ContratC(modele);
+                break;
+            case 'D':
+                contractHolder = new ContratD(modele);
+                break;
+            case 'E' :
+                contractHolder = new ContratE(modele);
+                break;
+        }
+        return contractHolder;
+    }
+}
---
+++ src/projetagile/ProjetSession.java
@@ -0,0 +1,51 @@
+/* main du projet*/
+package projetagile;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import projetagile.jsonmodels.ModeleJsonIn;
+import projetagile.jsonmodels.ModeleJsonOut;
+
+
+/**
+ *
+ * @author kf891141
+ */
+public class ProjetSession {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        if(args.length == 2){
+
+            String inputFilePath = args[0];
+            String outputFilePath = args[1];
+
+            lectureEtEcritureFichier(inputFilePath, outputFilePath);
+        } else {
+            System.out.println("Il y a une erreur avec les arguments");
+        }       
+        
+    }
+
+    private static void lectureEtEcritureFichier(String inputFilePath, String outputFilePath) {
+        try{
+            ModeleJsonIn reclamation = JsonFileHandler.ouvrireFichier(inputFilePath);
+            InterfaceContrat nouveauContrat = ContratFactory.instancieContrat(reclamation);
+            ModeleJsonOut test = nouveauContrat.calculRemboursement();
+            JsonFileHandler.ecrireFichier(outputFilePath, test);
+        } catch (InvalidArgumentException e) {
+            //TODO WRITE TO ERROR FILE
+            JsonFileHandler.ecrireFichierErreur(outputFilePath);
+            System.exit(0);
+        }
+    }
+    
+}
